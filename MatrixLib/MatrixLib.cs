5f48805947537451b041f7eb81347e18d65ce027
Emir Yusuf Topbas <emiryusuftopbas@gmail.com>
2019-11-25 16:12:52 +0300

    Initial Commit

---
+++ MatrixLib/MatrixLib.cs
@@ -0,0 +1,492 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixLib
+{
+    class RowLengthException : Exception
+    {
+        public RowLengthException(string message) : base(message)
+        {
+
+        }
+    }
+    class ColumnLengthException : Exception
+    {
+        public ColumnLengthException(string message) : base(message)
+        {
+
+        }
+    }
+    class MatrixIndexOutOfBoundsException : Exception
+    {
+        public MatrixIndexOutOfBoundsException(string message) : base(message)
+        {
+
+        }
+    }
+    class MatrixAdditionException : Exception
+    {
+        public MatrixAdditionException(string message) : base(message)
+        {
+            message = "You cannot add matrices that are not equal";
+        }
+    }
+    class MatrixSubtractionException : Exception
+    {
+        public MatrixSubtractionException(string message) : base(message)
+        {
+            message = "You cannot subtract matrices that are not equal";
+        }
+    }
+    class MatrixMultiplicationException : Exception
+    {
+        public MatrixMultiplicationException(string message) : base(message)
+        {
+
+        }
+    }
+    class MatrixFormatException : Exception
+    {
+        public MatrixFormatException(string message) : base(message)
+        {
+
+        }
+    }
+    class MatrixIsNotSquareException : Exception
+    {
+        public MatrixIsNotSquareException(string message) : base(message)
+        {
+
+        }
+    }
+    class ArrayCanNotConvertibleException : Exception
+    {
+        public ArrayCanNotConvertibleException(string message) : base(message)
+        {
+
+        }
+    }
+    public class Matrix
+    {
+
+        int _rowLength;
+        int _colLength;
+        double[,] matrixArr;
+
+        /// <summary>
+        /// Returns row length of the matrix
+        /// </summary>
+        /// <returns>Row length of the matrix.</returns>
+        public int RowLength
+        {
+            get
+            {
+                return _rowLength;
+            }
+
+        }
+
+        /// <summary>
+        /// Returns column length of the matrix
+        /// </summary>
+        /// <returns>Column length of the matrix.</returns>
+        public int ColLength
+        {
+            get
+            {
+                return _colLength;
+            }
+        }
+
+        private void SetColLength(int value)
+        {
+            if (value < 0)
+            {
+                throw new ColumnLengthException("must be greater than or equal to zero");
+            }
+            else
+            {
+                _colLength = value;
+            }
+        }
+        private void SetRowLength(int value)
+        {
+            if (value < 0)
+            {
+                throw new RowLengthException("must be greater than or equal to zero");
+            }
+            else
+            {
+                _rowLength = value;
+            }
+        }
+
+        /// <summary>
+        /// Creates matrix.
+        /// </summary>
+        /// <param name="rowLength">Row length.</param>
+        /// <param name="colLength">Column length.</param>
+        public Matrix(int rowLength, int colLength)
+        {
+            SetRowLength(rowLength);
+            SetColLength(colLength);
+
+            matrixArr = new double[this.RowLength, this.ColLength];
+        }
+
+        /// <summary>
+        /// Creates matrix from given string.
+        /// </summary>
+        /// <param name="query">Matlab like matrix definition sytantax [ ; ;] ';' represents rows.</param>
+        public Matrix(string query)
+        {
+            string[] splittedToSBO = query.Split('[');
+            string[] splittedToSBC = splittedToSBO[1].Split(']');
+            string[] splittedToSemiColon = splittedToSBC[0].Split(';');
+            if (splittedToSBO.Length == 2 && splittedToSBO.Length == 2 && CheckRowColFormat(splittedToSemiColon))
+            {
+                SetRowLength(splittedToSemiColon.Length);
+                SetColLength(splittedToSemiColon[0].Split(' ').Length);
+
+                matrixArr = new double[RowLength, ColLength];
+
+                for (int i = 0; i < RowLength; i++)
+                {
+                    string[] splittedToSpace = splittedToSemiColon[i].Split(' ');
+
+                    for (int j = 0; j < ColLength; j++)
+                    {
+                        matrixArr[i, j] = Convert.ToDouble(splittedToSpace[j]);
+                    }
+                }
+            }
+            else
+            {
+                throw new MatrixFormatException("Matrix not in desired format");
+            }
+        }
+
+        /// <summary>
+        /// Creates matrix from two-dimensional double array.
+        /// </summary>
+        /// <param name="matrix">two-dimensional double array.</param>
+        public Matrix(double[,] matrix)
+        {
+            SetRowLength(matrix.GetLength(0));
+            SetColLength(matrix.GetLength(0));
+            matrixArr = new double[RowLength, ColLength];
+            if (matrix.Length == (matrix.GetLength(0) * matrix.GetLength(1)))
+            {
+                for (int i = 0; i < RowLength; i++)
+                {
+                    for (int j = 0; j < ColLength; j++)
+                    {
+                        matrixArr[i, j] = matrix[i, j];
+                    }
+                }
+            }
+            else
+            {
+                throw new MatrixIsNotSquareException("Array can not convertible exception");
+            }
+
+        }
+
+        private bool CheckRowColFormat(string[] splittedToSemiColon)
+        {
+            int rowLength = splittedToSemiColon.Length;
+
+            int[] columnLengths = new int[rowLength];
+            List<bool> convertibleOrNot = new List<bool>();
+            double fortest;
+            for (int i = 0; i < rowLength; i++)
+            {
+                columnLengths[i] = splittedToSemiColon[i].Split(' ').Length;
+                for (int j = 0; j < splittedToSemiColon[i].Split(' ').Length; j++)
+                {
+                    string el = splittedToSemiColon[i].Split(' ')[j];
+                    convertibleOrNot.Add(double.TryParse(el, out fortest));
+
+                }
+            }
+
+            if ((columnLengths.Max() == columnLengths.Max()) && !convertibleOrNot.Contains(false))
+                return true;
+            return false;
+        }
+
+
+        public double this[int rowIndex, int colIndex]
+        {
+            set
+            {
+                matrixArr[rowIndex - 1, colIndex - 1] = value;
+            }
+            get
+            {
+                return matrixArr[rowIndex - 1, colIndex - 1];
+            }
+        }
+
+        /// <summary>
+        /// Returns transpose of the matrix.
+        /// </summary>
+        /// <returns>Transpose of matrix </returns>
+        public Matrix Transpose()
+        {
+            Matrix result = new Matrix(ColLength, RowLength);
+            for (int i = 1; i <= RowLength; i++)
+            {
+                for (int j = 1; j <= ColLength; j++)
+                {
+                    result[i, j] = this[j, i];
+
+                }
+            }
+            return result;
+        }
+
+
+        double DetCalc(int n, double[,] matrix)
+        {
+            double d = 0;
+            int subi, subj;
+
+            double[,] subMatrix = new double[n, n];
+
+            if (n == 1)
+            {
+                return matrix[0, 0];
+            }
+            else if (n == 2)
+            {
+                return ((matrix[0, 0] * matrix[1, 1]) - (matrix[1, 0] * matrix[0, 1]));
+            }
+            else
+            {
+                for (int k = 0; k < n; k++)
+                {
+                    subi = 0;
+                    for (int i = 1; i < n; i++)
+                    {
+                        subj = 0;
+                        for (int j = 0; j < n; j++)
+                        {
+                            if (j == k)
+                            {
+                                continue;
+                            }
+                            subMatrix[subi, subj] = matrix[i, j];
+                            subj++;
+                        }
+                        subi++;
+                    }
+                    d = d + (Math.Pow(-1, k) * matrix[0, k] * DetCalc(n - 1, subMatrix));
+                }
+            }
+            return d;
+        }
+
+        /// <summary>
+        /// Determinant calculator function.
+        /// </summary>
+        /// <returns>Determinant of the matrix</returns>
+        public double Det()
+        {
+            if (ColLength != RowLength)
+                throw new MatrixIsNotSquareException("Matrix must be square matrix");
+            else
+                return DetCalc(Convert.ToInt32(Math.Sqrt(matrixArr.Length)), matrixArr);
+        }
+
+
+
+        /// <summary>
+        /// Adds rows to matrix.
+        /// </summary>
+        /// <param name="rowIndex">Id of the row to add.</param>
+        /// <param name="rowArr">Double array of row elements.</param>
+        /// <returns>Nothing.</returns>
+        public void AddAsRow(int rowIndex, double[] rowArr)
+        {
+            if ((rowIndex > RowLength) || (rowIndex <= 0) || (rowArr.Length > ColLength))
+            {
+                throw new MatrixIndexOutOfBoundsException("");
+            }
+            else
+            {
+                for (int i = 0; i < rowArr.Length; i++)
+                {
+                    matrixArr[rowIndex - 1, i] = rowArr[i];
+                }
+            }
+
+        }
+        /// <summary>
+        /// Adds columns to matrix.
+        /// </summary>
+        /// <param name="rowIndex">Id of the column to add.</param>
+        /// <param name="rowArr">Double array of column elements.</param>
+        /// <returns>Nothing.</returns>
+        public void AddAsColumn(int colIndex, double[] colArr)
+        {
+            if ((colIndex > ColLength) || (colIndex <= 0) || (colArr.Length > ColLength))
+            {
+                throw new MatrixIndexOutOfBoundsException("");
+            }
+            else
+            {
+                for (int i = 0; i < colArr.Length; i++)
+                {
+                    matrixArr[i, colIndex - 1] = colArr[i];
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns a double array that represents the current matrix.
+        /// </summary>
+        /// <returns>A double array that represents the current matrix.</returns>
+        public double[,] ToDoubleArray()
+        {
+            return matrixArr;
+        }
+
+        /// <summary>
+        /// Returns a string that represents the current matrix.
+        /// </summary>
+        /// <returns>A string that represents the current matrix..</returns>
+        public override string ToString()
+        {
+            StringBuilder str = new StringBuilder();
+            for (int i = 0; i < RowLength; i++)
+            {
+                str.Append("| ");
+                for (int j = 0; j < ColLength; j++)
+                {
+                    str.Append(matrixArr[i, j] + " ");
+                }
+                str.Append("|");
+
+                str.Append("\n");
+            }
+            return str.ToString();
+        }
+
+        public override bool Equals(Object obj)
+        {
+
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                Matrix m1 = (Matrix)obj;
+                bool isRowLengthEqual = this.RowLength == m1.RowLength;
+                bool isColLengthEqual = this.ColLength == m1.RowLength;
+                List<bool> isElementsEqual = new List<bool>();
+                for (int i = 1; i <= RowLength; i++)
+                {
+                    for (int j = 1; j <= ColLength; j++)
+                    {
+                        if (this[i, j] == m1[i, j])
+                            isElementsEqual.Add(true);
+                        else
+                            isElementsEqual.Add(false);
+                    }
+                }
+                return isRowLengthEqual && isColLengthEqual && !isElementsEqual.Contains(false);
+
+            }
+        }
+
+        public static Matrix operator +(Matrix m1, Matrix m2)
+        {
+            Matrix result = new Matrix(m1.RowLength, m2.ColLength);
+            if (!(m1.RowLength == m2.RowLength) || !(m1.ColLength == m2.ColLength))
+            {
+                throw new MatrixAdditionException("");
+            }
+            else
+            {
+                for (int i = 1; i <= m1.RowLength; i++)
+                {
+                    for (int j = 1; j <= m1.ColLength; j++)
+                    {
+                        result[i, j] = m1[i, j] + m2[i, j];
+
+
+                    }
+                }
+                return result;
+            }
+        }
+        public static Matrix operator -(Matrix m1, Matrix m2)
+        {
+            Matrix result = new Matrix(m1.RowLength, m2.ColLength);
+            if (!(m1.RowLength == m2.RowLength) || !(m1.ColLength == m2.ColLength))
+            {
+                throw new MatrixSubtractionException("");
+            }
+            else
+            {
+                for (int i = 1; i <= m1.RowLength; i++)
+                {
+                    for (int j = 1; j <= m1.ColLength; j++)
+                    {
+                        result[i, j] = m1[i, j] - m2[i, j];
+                    }
+                }
+                return result;
+            }
+        }
+
+
+        public static Matrix operator *(Matrix m1, Matrix m2)
+        {
+            if (!(m1.ColLength == m2.RowLength))
+            {
+                throw new MatrixMultiplicationException("");
+            }
+            else
+            {
+                Matrix result = new Matrix(m1.RowLength, m2.RowLength);
+
+                for (int i = 1; i <= m1.RowLength; i++)
+                {
+                    for (int j = 1; j <= m2.ColLength; j++)
+                    {
+                        result[i, j] = 0;
+                        for (int r = 1; r <= m1.ColLength; r++)
+                        {
+                            result[i, j] += m1[i, r] * m2[r, j];
+                        }
+                    }
+                }
+
+                return result;
+            }
+        }
+        public static Matrix operator *(Matrix m1, double number)
+        {
+
+            Matrix result = new Matrix(m1.RowLength, m1.ColLength);
+
+            for (int i = 1; i <= m1.RowLength; i++)
+            {
+                for (int j = 1; j <= m1.ColLength; j++)
+                {
+                    result[i, j] = Convert.ToDouble(m1[i, j]) * number;
+                }
+            }
+
+            return result;
+        }
+
+
+    }
+}
---
+++ readme.md
@@ -0,0 +1,199 @@
+# MatrixLib
+MatrixLib is a c# library that helps you to create matrices and do matrix calculations in c#. It is very simple to use.
+
+## How to add 
+You need to download the dll , or you can build the dll from the source code.
+
+ - Create a project 
+ - Open solution explorer menu
+ ![Add reference](https://i.ibb.co/y4XJZFK/Annotation-2019-11-24-170306.png)
+ - Right click on references and click *Add Reference*
+ -![Reference Manager](https://i.ibb.co/Tqg3NdJ/Annotation-2019-11-24-170444.png)
+ - Click *Browse*
+![Select the files to reference](https://i.ibb.co/PzzSpkc/Annotation-2019-11-24-170600.png)
+ - Select **MatrixLib.dll** and click *Add*
+ ![enter image description here](https://i.ibb.co/rtj1Jpb/Annotation-2019-11-24-170655.png)
+ - We have successfully added the library .
+ - Don't forget the include the library.
+ - ![enter image description here](https://i.ibb.co/GR90WGW/Annotation-2019-11-24-171947.png)
+ - Now we are ready to use the library
+
+## Create matrix
+
+There are different ways to create matrix , but one of them is 
+### Empty matrix creation
+   ```csharp
+   Matrix matrix = new Matrix(3,3);
+```
+We specified the row length and column length of the matrix. 
+After doing that we can add rows and columns. Adding rows and columns will be explained later.
+### Matlab like matrix creation
+```csharp
+   Matrix matrix = new Matrix("[12 6 3;15 56 88;55 32 18]");
+```
+Created matrix will look like:
+![enter image description here](https://i.ibb.co/H7t95B7/Annotation-2019-11-24-173412.png)
+You have to type ';' semicolon after row.
+You have to type space after element.
+Typing unnecessary spaces causes error.
+
+If the string is not in desired format this will throw **MatrixFormatException**
+You can catch it :)
+
+### Create matrix from two dimensional double array
+```csharp
+   double[,] twoDimensionalArr = new double[2, 2] {
+    {80,9 },
+    {16,71 }
+    };
+    Matrix matrix = new Matrix(twoDimensionalArr);
+    
+```
+Created matrix will look like:
+![enter image description here](https://i.ibb.co/txzfss7/Annotation-2019-11-24-173412.png)
+
+## Adding rows and columns
+
+There are 2 functions to adding rows and columns;
+
+**Adding as rows**
+*AddAssRow(int index, double [] arr)*
+
+```csharp
+   Matrix matrix = new Matrix(3,3);
+   double[] row1 = { 1, 6, 6 };
+   double[] row2 = { 5, 7, 3 };
+   double[] row3 = { 3, 4, 8 };
+   matrix.AddAsRow(1 , row1);
+   matrix.AddAsRow(2 , row2);
+   matrix.AddAsRow(3 , row3)
+    
+```
+Matrix will look like:
+![enter image description here](https://i.ibb.co/47RkN7D/Annotation-2019-11-24-203554.png)
+***Adding as columns***
+*AddAssColumn(int index, double [] arr)*
+```csharp
+ Matrix matrix = new Matrix(3,3);
+ double[] col1 = { 7, 4, 1 };
+ double[] col2 = { 4, 5, 7 };
+ double[] col3 = { 9, 8, 6 };
+ matrix.AddAsColumn(1 , col1);
+ matrix.AddAsColumn(2 , col2);
+ matrix.AddAsColumn(3,  col3);
+    
+```
+Matrix will look like:
+![enter image description here](https://i.ibb.co/0qyQ1dv/Annotation-2019-11-24-204104.png)
+
+## Matrix row length and column length
+You may want to get row length and column length of the matrix.
+There are two properties for that;
+```csharp
+Matrix matrix = new Matrix("[12 6 3;15 56 88;55 32 18]");
+Console.WriteLine("Row length of the matrix is = " + matrix.RowLength);
+Console.WriteLine("Column length of the matrix is = " + matrix.ColLength);  
+```
+Output is : 
+![enter image description here](https://i.ibb.co/Ns591vQ/Annotation-2019-11-24-204104.png)
+
+## Displaying matrices
+There are ToString() method for displaying matrices.
+```csharp
+Matrix matrix = new Matrix("[12 6 3;15 56 88;55 32 18]");
+Console.WriteLine(matrix.ToString());
+```
+Output is:
+![enter image description here](https://i.ibb.co/PmP3byR/Annotation-2019-11-24-204104.png)
+
+## Getting and setting matrix elements
+We can get and set matrix elements using indexer.
+
+Getting matrix element
+
+```csharp
+Matrix matrix = new Matrix("[12 6 3;15 56 88;55 32 18]");
+Console.WriteLine(matrix[1,1]);
+// this will output the first row and first column element (12)
+```
+Setting matrix element
+```csharp
+Matrix matrix = new Matrix("[12 6 3;15 56 88;55 32 18]");
+matrix[2, 3] = 42;
+ //this will change the second row and third column element to (42)
+```
+
+## Matrix addition , subtraction and multiplication
+
+You can simply add or subtract matrices like int or double.
+### Addition
+```csharp
+Matrix m1 = new Matrix("[12 6 3;15 56 88;55 32 18]");
+ Matrix m2 = new Matrix("[8 5 4;12 55 12;43 18 12]");
+Matrix m3 = m2 + m1;
+Console.WriteLine(m3.ToString());
+```
+if the row and column lengths of the matrices are not equal this will throw **MatrixAdditionException**
+
+Output is:
+![enter image description here](https://i.ibb.co/PcGBcHL/Annotation-2019-11-24-212147.png)
+### Subtraction
+```csharp
+Matrix m1 = new Matrix("[12 6 3;15 56 88;55 32 18]");
+Matrix m2 = new Matrix("[8 5 4;12 55 12;43 18 12]");
+Matrix m3 = m2 - m1;
+Console.WriteLine(m3.ToString());
+```
+if the row and column lengths of the matrices are not equal this will throw **MatrixSubtractionException**
+Output is:
+![enter image description here](https://i.ibb.co/qFqsYqy/Annotation-2019-11-24-212147.png)
+### Multiplication
+```csharp
+Matrix m1 = new Matrix("[1 2;2 0;3 1]");
+Matrix m2 = new Matrix("[4 3;1 2]");
+Matrix m3 = m1* m2;
+Console.WriteLine(m3.ToString());
+```
+if the number of columns of the first matrix is ​​not equal to the number of rows of the second matrix this will throw **MatrixMultiplicationException**
+Output is :
+![enter image description here](https://i.ibb.co/QcV7PPd/Annotation-2019-11-24-212147.png)
+#### Multiplication matrix with scalar number
+```csharp
+   Matrix m1 = new Matrix("[1 2;2 0;3 1]");
+   Matrix m3 = m1* 2;
+   Console.WriteLine(m3.ToString());
+```
+Output is:
+![enter image description here](https://i.ibb.co/7SWkVyn/Annotation-2019-11-24-212147.png)
+
+## Transpose of the matrix
+
+You can easily get the transpose of the matrix using Transpose(); method.
+```csharp
+   Matrix m1 = new Matrix("[2 6 7;3 6 2;4 6 5]");
+   Matrix m2 = m1.Transpose();
+   Console.WriteLine(m2.ToString());
+```
+Output is :
+![enter image description here](https://i.ibb.co/XYzyj36/Annotation-2019-11-24-221247.png)
+## Determinant of the matrix
+There is Det(); method for calculate determinant.
+```csharp
+   Matrix m1 = new Matrix("[1 5 3;2 4 7;4 6 2]");          
+   Console.WriteLine(m1.Det());
+   // output is (74)
+```
+If matrix is not square this will throw **MatrixIsNotSquareException**
+## Turning matrix into two dimensional double array
+
+```csharp
+   Matrix m1 = new Matrix("[1 5 3;2 4 7;4 6 2]");
+   double[,] arr = m1.ToDoubleArray();
+   
+```
+## Contributors
+
+ - Emir Yusuf Topbaş <emiryusuftopbas@gmail.com>
+## License & copyright
+Licensed under the [MIT License](LICENSE)
+
